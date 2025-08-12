cf21bc0b7bf988405d9242de97bca557971f0bc0
joeltariku <jojotk10@gmail.com>
2025-08-12 18:22:55 -0400

    redesigned folder structure

--- mongo.js
+++ mongo.js
@@ -1,11 +1,19 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require('mongoose')
+require('dotenv').config()
 
-const password = process.env.MONGO_PASSWORD;
+// if (process.argv.length < 3) {
+//   console.log('give password as argument')
+//   process.exit(1)
+// }
 
-const url = `mongodb+srv://jojotk10:${password}@cluster0.k8jfzsn.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+// const password = process.argv[2]
+console.log('password: ', process.env.MONGO_PASSWORD)
 
-mongoose.set('strictQuery',false)
+console.log('Connecting to MongoDB...')
+
+const url = `mongodb+srv://jojotk10:${process.env.MONGO_PASSWORD}@cluster0.k8jfzsn.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
@@ -13,14 +21,22 @@ const noteSchema = new mongoose.Schema({
   important: Boolean,
 })
 
-noteSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
-})
-
 const Note = mongoose.model('Note', noteSchema)
 
-module.exports = Note;
+
+const note2 = new Note({
+  content: 'Browser can execute only JavaScript',
+  important: false,
+})
+
+note2.save().then(() => {
+  console.log('note2 saved!')
+  mongoose.connection.close()
+})
+
+Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
